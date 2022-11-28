fe9c7e6d749bc0bec7215cea08c6d9b99d40fd6c
Laurence1978 <113360221+Laurence1978@users.noreply.github.com>
2022-11-28 16:53:38 +0100

    Add files via upload

---
+++ my_streamlit_app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pandas as pd
+import requests 
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+###### titre de la page
+
+st.title("Cars Analysis")
+st.image("https://static.vecteezy.com/system/resources/previews/000/524/893/non_2x/vector-car-set.jpg")
+
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
+df_cars = pd.read_csv(link)
+st.write(df_cars)
+
+st.title('Graphique interactif de distribution')
+
+continent = st.radio("Choix de la zone géographique pour afficher les voitures selon leur cylindre", ("US", "Europe", "Japon"))
+if continent == "US":
+    dp_cars = sns.countplot(df_cars[df_cars['continent'].str.contains('US.')]['cylinders'])
+    st.pyplot(dp_cars.figure)
+    st.write('Voici le graphique filtré sur les US.')
+    st.write('Le nombre moyen de cylindres est :', round(df_cars[df_cars['continent'].str.contains('US.')]['cylinders'].mean(),2))
+elif continent == "Europe" :
+    dp_cars2 = sns.countplot(df_cars[df_cars['continent'].str.contains('Europe.')]['cylinders'])
+    st.pyplot(dp_cars2.figure)
+    st.write("Voici le graphique filtré sur l'Europe.")
+    st.write('Le nombre moyen de cylindres est :', round(df_cars[df_cars['continent'].str.contains('Europe.')]['cylinders'].mean(),2))
+elif continent == "Japon":
+    dp_cars3 = sns.countplot(df_cars[df_cars['continent'].str.contains('Japan.')]['cylinders'])
+    st.pyplot(dp_cars3.figure)
+    st.write("Voici le graphique filtré sur le Japon.")
+    st.write('Le nombre moyen de cylindres est :', round(df_cars[df_cars['continent'].str.contains('Japan.')]['cylinders'].mean(),2))
+    
+       
+    
+st.title('Graphique de corrélations filtré par région')
+
+
+continent2 = st.radio("Choix de la zone géographique pour la heatmap de corrélations :", ("US", "Europe", "Japon"))
+if continent2 == "US":
+    hm_cars = sns.heatmap(df_cars[df_cars['continent'].str.contains('US.')].corr(), center=0, cmap=sns.color_palette("vlag", as_cmap=True))
+    st.pyplot(hm_cars.figure, clear_figure = True)
+    st.write('Voici la heatmap filtré sur les US.')
+elif continent2 == "Europe" :
+    hm_cars2 = sns.heatmap(df_cars[df_cars['continent'].str.contains('Europe.')].corr(), center=0, cmap=sns.color_palette("vlag", as_cmap=True))
+    st.pyplot(hm_cars2.figure, clear_figure = True)
+    st.write("Voici la heatmap filtrée sur l'Europe.")
+elif continent2 == "Japon":
+    hm_cars3 = sns.heatmap(df_cars[df_cars['continent'].str.contains('Japan.')].corr(), center=0, cmap=sns.color_palette("vlag", as_cmap=True))
+    st.pyplot(hm_cars3.figure, clear_figure = True)
+    st.write("Voici la heatmap filtrée sur le Japon.")
+    st.write("Pour les 3 régions, 4 colonnes sont positivement corrélées (en rouge).")
